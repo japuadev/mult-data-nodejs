7debd1d5ed85e6470cd0f60b15b4a44c073745f3
japuamelo <dimeloarquitetura@gmail.com>
2022-05-26 21:52:13 -0300

    Adicionando implementações das rotas

--- src/api.js
+++ src/api.js
@@ -2,21 +2,20 @@ const Hapi = require('hapi')
 const Context = require('./db/strategies/base/contextStrategy')
 const MongoDB = require('./db/strategies/mongodb/mongodb')
 const HeroiSchema = require('./db/strategies/mongodb/schemas/herosSchema')
+const HeroRoutes = require('./routes/heroRoutes')
 const app = new Hapi.Server({
     port: 5000
 })
+
+function mapRoutes(instance, methods) {
+    return methods.map(method => instance[method]())
+}
 
 async function main() {
     const connection = MongoDB.connect()
     const context = new Context(new MongoDB(connection, HeroiSchema))
     app.route([
-        {
-            path: '/heroes',
-            method: 'GET',
-            handler: (request, head) => {
-                return context.read()
-            }
-        }
+        ...mapRoutes
     ])
 
     await app.start()
